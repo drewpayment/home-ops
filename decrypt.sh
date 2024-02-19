6419d2e3406df416e28152740ec5e91bcc9eaadf
drewpayment <drew.payment@gmail.com>
2024-02-18 19:39:11 -0500

    🔒 chore(sops): add new creation rule for secret/*.sops.yaml files 🆕 feat(decrypt.sh): add new script to decrypt sops files 🔒 feat(dockerconfigjson.sops.yaml): add new encrypted dockerconfigjson secret 🔧 fix(helmrelease.yaml): add imagePullSecrets to helm release to pull from private registry
    🔒 chore(secret.sops.yaml): update encrypted database credentials
    
    🔄 Update encrypted values for DATABASE_URL, MYSQL_DB_HOST, MYSQL_DB_USER, and MYSQL_DB_PASSWORD in secret.sops.yaml. This is necessary to reflect the changes in the database credentials.

---
+++ decrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# if first argument is null exit
+if [ -z "$1" ]; then
+  echo "Please provide a file to decrypt!"
+  exit 1
+fi
+
+export SOPS_AGE_KEY_FILE=$(pwd)/age.key
+
+sops --decrypt --in-place $1
+
+exit 0
